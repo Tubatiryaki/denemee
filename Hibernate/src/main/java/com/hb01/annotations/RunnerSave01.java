f456f8b188fd5d41f7b31ff9789fd35612f59c2f
ebut-dev <67825399+ebut-dev@users.noreply.github.com>
2022-05-21 15:00:06 +0300

    Hibernate first class commit

---
+++ Hibernate/src/main/java/com/hb01/annotations/RunnerSave01.java
@@ -0,0 +1,23 @@
+package com.hb01.annotations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class RunnerSave01 {
+	public static void main(String[] args) {
+		Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student01.class);
+		SessionFactory sf=con.buildSessionFactory();
+		Session session=sf.openSession();
+		
+		//
+		
+		
+		
+		session.close();
+		sf.close();
+	}
+
+}
