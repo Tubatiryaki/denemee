e194e6a5f06378c75658ab87adc908e23c7a5ab1
ebut-dev <67825399+ebut-dev@users.noreply.github.com>
2022-05-21 23:44:06 +0300

    Hibernate save, getQueries: SQL, HQL

--- Hibernate/src/main/java/com/hb01/annotations/Student01.java
+++ Hibernate/src/main/java/com/hb01/annotations/Student01.java
@@ -3,15 +3,27 @@ package com.hb01.annotations;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 //Veritabanýnda bir tablo karþýlýðý olduðunu gösterir.
+//Entity annotation içine name ile bir isim verirseniz 
+//tablo isminiz ve sorgularda kullanacaðýnýz entity ismi bu verilen isim olacaktýr.
+//@Entity(name="student01")
 @Entity
+//Table annotation içinde name propertyisi oluþacak tablonun ismini belirler.
+//@Table(name="tbl_student01")
 public class Student01 {
 
 	//@Id annotation altýndaki deðiþkenin  primary key yapar.
+	//Bu annotasyon her Entity sýnýfýnda olmalý  
 	@Id
 	private int id;
 	
+	
+	/*
+	 * Eðer tablodaki kolon ismini deðiþken isminden farklý bir isim olarak
+	 * oluþturmak istersek @Column(name="student_name") bu þekilde kullanabiliriz.
+	 */
 	
 	@Column
 	private String name;
